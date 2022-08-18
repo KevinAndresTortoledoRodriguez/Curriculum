7ef414a9a6a560d53262ef4abde11f50d34e04df
Kevin Tortoledo <86644591+KevinAndresTortoledoRodriguez@users.noreply.github.com>
2022-08-18 18:04:08 -0500

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import { Container, Row, Col } from 'react-bootstrap'
+import './App.css'
+function App() {
+  return (
+    <Row className='body'>
+      <Container className="Contenedor">
+        <Row>
+            <Col>
+              holis
+            </Col>
+        </Row>    
+      </Container>
+    </Row>
+  );
+}
+
+export default App;
